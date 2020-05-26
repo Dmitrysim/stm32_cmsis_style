86d4a593a810838ffa3c69fa34a8e6dc5b7f20c2
Dmitrysim <dima_87771995@mail.ru>
2020-05-26 20:16:11 +0300

    program timers

--- main.c
+++ main.c
@@ -5,22 +5,22 @@ void delay_ms(uint16_t ms);
 
 typedef struct
 {
-	uint16_t cnt; // счетчик, мс
-	uint16_t time; // время счета, мс
-	uint8_t flag; // 1 - таймер сработал
+	uint16_t cnt; 	// counter, ms
+	uint16_t time; 	// time of count, ms
+	uint8_t  flag; 	// 1 - timer worked
 } soft_tim;
 
-soft_tim st_LED_PD12 = {0, 100, 0};
+soft_tim st_LED_PD12 = {0, 100,  0};
 soft_tim st_LED_PD13 = {0, 2000, 0};
 //soft_tim st_LED_PD15 = {0, 12, 0};
-soft_tim st_LED_PD15 = {0, 28, 0};
+soft_tim st_LED_PD15 = {0, 28,   0};
 
 typedef struct
 {
-	uint16_t cnt; // счетчик, мс
-	uint16_t time; // время счета, мс
-	uint8_t flag; // 1 - таймер сработал
-	uint8_t en; // 1 - работа разрешена
+	uint16_t cnt; 	// counter, ms
+	uint16_t time; 	// time of count, ms
+	uint8_t  flag; 	// 1 - timer worked
+	uint8_t  en; 	// 1 - work enable
 } soft_tim2;
 
 soft_tim2 st_LED_PD14 = {0, 100, 0, 0};
@@ -40,10 +40,10 @@ int main (void)
 		
 		/*
 		GPIOD->ODR |= GPIO_ODR_ODR_12; // 1
-		//for(i=0; i<0xFFFFF; i++) {} // задержка
+		//for(i=0; i<0xFFFFF; i++) {} // delay
 		delay_ms(100);
 		GPIOD->ODR &= ~GPIO_ODR_ODR_12; // 1
-		//for(i=0; i<0xFFFFF; i++) {} // задержка
+		//for(i=0; i<0xFFFFF; i++) {} // delay
 		delay_ms(100);
 		*/
 		if(st_LED_PD12.flag)
@@ -64,7 +64,7 @@ int main (void)
 		}
 
 		/*
-		// яркость от 0 до макс за 3 сек. - 3/256 = 12 мс
+		// brightness from 0 untill max for 3 sec. - 3/256 = 12 ms
 		if(st_LED_PD15.flag)
 		{
 			st_LED_PD15.flag = 0;
@@ -76,7 +76,7 @@ int main (void)
 		}
 		*/
 		
-		// серво от 1 до 2 мс макс за 5 сек. - 5/180 = 28 мс
+		// servo from 1 untill 2 ms max for 5 sec. - 5/180 = 28 ms
 		if(st_LED_PD15.flag)
 		{
 			st_LED_PD15.flag = 0;
@@ -94,31 +94,31 @@ int main (void)
 //////////////////////////////////////////////////////////////////////////////
 																	//////////
 
-// обработчик прерывания
+// interrupt handler
 void TIM7_IRQHandler(void)
 {
-	TIM7->SR &= ~TIM_SR_UIF; // сброс флага
+	TIM7->SR &= ~TIM_SR_UIF; // reset flag
 	//GPIOD->ODR ^= GPIO_ODR_ODR_12;
-	if(st_LED_PD12.cnt > 0) st_LED_PD12.cnt--; // уменьшение счетчика
+	if(st_LED_PD12.cnt > 0) st_LED_PD12.cnt--; 		// decrease counter
 	else
 	{
-		st_LED_PD12.cnt = st_LED_PD12.time; // перезагрузка счетчика
+		st_LED_PD12.cnt = st_LED_PD12.time; 		// counter overload
 		st_LED_PD12.flag = 1;
 	}
 
-	if(st_LED_PD13.cnt > 0) st_LED_PD13.cnt--; // уменьшение счетчика
+	if(st_LED_PD13.cnt > 0) st_LED_PD13.cnt--; 		// decrease counter
 	else
 	{
-		st_LED_PD13.cnt = st_LED_PD13.time; // перезагрузка счетчика
+		st_LED_PD13.cnt = st_LED_PD13.time; 		// counter overload
 		st_LED_PD13.flag = 1;
 	}
 
 	if(st_LED_PD14.en)
 	{
-		if(st_LED_PD14.cnt > 0) st_LED_PD14.cnt--; // уменьшение счетчика
+		if(st_LED_PD14.cnt > 0) st_LED_PD14.cnt--; 	// decrease counter
 		else
 		{
-			st_LED_PD14.cnt = st_LED_PD14.time; // перезагрузка счетчика
+			st_LED_PD14.cnt = st_LED_PD14.time; 	// counter overload
 			st_LED_PD14.flag = 1;
 		}
 	}
@@ -127,10 +127,10 @@ void TIM7_IRQHandler(void)
 		GPIOD->ODR &= ~GPIO_ODR_ODR_14;
 	}
 
-	if(st_LED_PD15.cnt > 0) st_LED_PD15.cnt--; // уменьшение счетчика
+	if(st_LED_PD15.cnt > 0) st_LED_PD15.cnt--; 		// decrease counter
 	else
 	{
-		st_LED_PD15.cnt = st_LED_PD15.time; // перезагрузка счетчика
+		st_LED_PD15.cnt = st_LED_PD15.time; 		// counter overload
 		st_LED_PD15.flag = 1;
 	}
 	
@@ -138,42 +138,42 @@ void TIM7_IRQHandler(void)
 
 void timer_settings(void) {
 	
-	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; // включили тактирование
-	//GPIOD->MODER |= GPIO_MODER_MODER12_0; // на выход
-	//GPIOD->MODER |= GPIO_MODER_MODER12_0 | GPIO_MODER_MODER13_0; // на выход
-	GPIOD->MODER |= GPIO_MODER_MODER12_0 | GPIO_MODER_MODER13_0 | GPIO_MODER_MODER14_0; // на выход
-	// 0: Output push-pull (reset state)
-	// 00: 2 MHz Low speed
+	RCC->AHB1ENR |= RCC_AHB1ENR_GPIODEN; 													// enabled timing
+	//GPIOD->MODER |= GPIO_MODER_MODER12_0; 												// output
+	//GPIOD->MODER |= GPIO_MODER_MODER12_0 | GPIO_MODER_MODER13_0; 							// output
+	GPIOD->MODER |= GPIO_MODER_MODER12_0 | GPIO_MODER_MODER13_0 | GPIO_MODER_MODER14_0; 	// output
+																							// 0: Output push-pull (reset state)
+																							// 00: 2 MHz Low speed
 	
-	// 16000000/PSC/ARR = частота срабатывания таймера
+	// 16000000/PSC/ARR = frequency of timer
 	// 1 ms - 1 kHz
 	// 16000000/PSC/ARR = 1000
 	// 16000000/16/1000 = 1000 < 65535
-	RCC->APB1ENR |= RCC_APB1ENR_TIM7EN; // вкл. тактирование
+	RCC->APB1ENR |= RCC_APB1ENR_TIM7EN; 													// enabled timing
 	TIM7->PSC = 16 - 1;
 	//TIM7->PSC = 16000 - 1;
 	TIM7->ARR = 1000;
-	TIM7->DIER |= TIM_DIER_UIE; // разр прер.
-	NVIC_EnableIRQ(TIM7_IRQn); // разр. прер. в NVIC
-	// глобальное разрешение прерываний 
-	TIM7->CR1 |= TIM_CR1_CEN; // включить таймер
+	TIM7->DIER |= TIM_DIER_UIE; 															// enabled of interrupt
+	NVIC_EnableIRQ(TIM7_IRQn); 																// enabled of interrupt NVIC
+	// global interrupt resolution 
+	TIM7->CR1 |= TIM_CR1_CEN; 																// enabled timer
 	
-	//ШИМ
-	GPIOD->MODER |= GPIO_MODER_MODER15_1; // альтернативная ф-я
-	GPIOD->AFR[1] |= 0x20000000; // AFR[0] = AFRL, AFR[1] = AFRH, TIM4_CH4 - AF2
-	//
-	// частота ШИМ  - 100 Hz
-	// ARR - период - 256 уровней яркости
+	//PWM
+	GPIOD->MODER |= GPIO_MODER_MODER15_1; 													// alternative function
+	GPIOD->AFR[1] |= 0x20000000; 															// AFR[0] = AFRL, AFR[1] = AFRH, TIM4_CH4 - AF2
+	
+	// Frequency PWM  - 100 Hz
+	// ARR - perid - 256 levels of brightness
 	// 16000000/PSC/ARR = 100
 	// 16000000/PSC/256 = 100
 	// 16000000/625/256 = 1000 < 65535
-	// серво машина
-	// частота ШИМ  - 50 Hz (период 20 мс)
-	// длительность импульса ~1мс(одно крайнее) - 1,5мс(среднее) - 2,0мс(второе крайнее)
-	// от 1 мс до 2 мс - 180 град.
-	// с точностью 1 град - 20мс * 180 = 3600
+	// servo
+	// Frequency PWM  - 50 Hz (period 20 ms)
+	// pulse duration ~1ms(first end) - 1,5ms(average) - 2,0ms(second end)
+	// from 1 ms untill 2 ms - 180 degr.
+	// with precision 1 degr - 20ms * 180 = 3600
 	// 16000000/PSC/3600 = 50
-	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN; // вкл. тактирование
+	RCC->APB1ENR |= RCC_APB1ENR_TIM4EN; 													// enabled timing
 	/*
 	TIM4->PSC = 625 - 1;
 	TIM4->ARR = 256;
@@ -182,9 +182,9 @@ void timer_settings(void) {
 	TIM4->ARR = 3600;
 	
 	// CC4S: Capture/Compare 1 selection - 00: CC4 channel is configured as output.
-	TIM4->CCMR2 |= TIM_CCMR2_OC4M_2 | TIM_CCMR2_OC4M_1; // 110: PWM mode 1
-	TIM4->CCER |= TIM_CCER_CC4E; // подключить сигнал к ножке
-	TIM4->CR1 |= TIM_CR1_CEN; // включить таймер
+	TIM4->CCMR2 |= TIM_CCMR2_OC4M_2 | TIM_CCMR2_OC4M_1; 									// 110: PWM mode 1
+	TIM4->CCER |= TIM_CCER_CC4E; 															// signal for pin
+	TIM4->CR1 |= TIM_CR1_CEN; 																// timer enable
 	//TIM4->CCR4 = 0;
 	TIM4->CCR4 = 180;
 	
@@ -192,18 +192,18 @@ void timer_settings(void) {
 
 void delay_ms(uint16_t ms)
 {
-	// 16000000/PSC/ARR = частота срабатывания таймера 
+	// 16000000/PSC/ARR = timer frequency 
 	// 1 ms - 1 kHz
 	// 16000000/PSC = 1000
 	// PSC = 16000000/1000 = 16000 < 65535
-	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN; // вкл. тактирование
-	TIM6->CR1 |= TIM_CR1_OPM; // однократн. срабат.
+	RCC->APB1ENR |= RCC_APB1ENR_TIM6EN; 													// enabled timing
+	TIM6->CR1 |= TIM_CR1_OPM; 																// single actuation
 	TIM6->PSC = 16000 - 1;
 	TIM6->ARR = ms;
-	TIM6->EGR |= TIM_EGR_UG; // переинициализация таймера
-	TIM6->SR &= ~ TIM_SR_UIF; // сбросим флаг
-	TIM6->CR1 |= TIM_CR1_CEN; // включить таймер
-	while(!(TIM6->SR & TIM_SR_UIF)) {} // ждем
+	TIM6->EGR |= TIM_EGR_UG; 																// re-initialization of the timer
+	TIM6->SR &= ~ TIM_SR_UIF; 																// reset the flag
+	TIM6->CR1 |= TIM_CR1_CEN; 																// timer enable
+	while(!(TIM6->SR & TIM_SR_UIF)) {} 														// wait
 	
 }
 
